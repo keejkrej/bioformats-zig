3ee606e30a541fb888b3f9e8e00edafec160038f
keejkrej <ctyjackcao@outlook.com>
2026-05-06 01:18:16 +0200

    Target public Flex fixture

--- fixtures/fetch.ps1
+++ fixtures/fetch.ps1
@@ -3,7 +3,7 @@ param(
     [string]$OutDir = "fixtures/cache",
     [int]$MaxDepth = 2,
     [long]$MaxBytes = 209715200,
-    [string]$NamePattern = '\.(tif|tiff|ome\.tiff|png|gif|bmp|jpg|jpeg|jp2|jpx|am|amiramesh|grey|hx|labels|dm2|dm3|dm4|c01|dib|dng|lsm|oir|vsi|ets|nd2|czi|lif|ics|ids|dv|r3d|mrc|map|nii|nrrd|nhdr|v|dcm|dicom|ima|vms|ims|ch5|h5|xml)$',
+    [string]$NamePattern = '\.(tif|tiff|ome\.tiff|png|gif|bmp|jpg|jpeg|jp2|jpx|am|amiramesh|grey|hx|labels|dm2|dm3|dm4|c01|dib|flex|mea|res|dng|lsm|oir|vsi|ets|nd2|czi|lif|ics|ids|dv|r3d|mrc|map|nii|nrrd|nhdr|v|dcm|dicom|ima|vms|ims|ch5|h5|xml)$',
     [switch]$List
 )
 
@@ -122,6 +122,7 @@ function Preferred-NamePattern {
         "amira" { return '\.(am|amiramesh|grey|hx|labels)$' }
         "cellomics" { return '\.(c01|dib)$' }
         "ecat7" { return '\.v$' }
+        "flex" { return '\.(flex|mea|res)$' }
         "gatan" { return '\.dm[34]$' }
         "gatandm2" { return '\.dm2$' }
         "hamamatsuvms" { return '\.vms$' }
